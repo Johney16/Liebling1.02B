9d12174cc3e7c4ecd47ef3cf84a5279e34d90861
Johney16 <john.esl100@gmail.com>
2021-04-08 23:32:31 -0600

    Update helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,34 @@
+onInitialise: function (status) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+    // enable cookies
+    }
+    if (type == 'opt-out' && !didConsent) {
+    // disable cookies
+    }
+    },
+    onStatusChange: function(status, chosenBefore) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+    // enable cookies
+    }
+    if (type == 'opt-out' && !didConsent) {
+    // disable cookies
+    }
+    },
+    onRevokeChoice: function() {
+    var type = this.options.type;
+    if (type == 'opt-in') {
+    // disable cookies
+    }
+    if (type == 'opt-out') {
+    // enable cookies
+    }
+    }
+
+
 export const isRTL = () => {
   const $html = document.querySelector('html')
   return ['ar', 'he', 'fa'].includes($html.getAttribute('lang'))
